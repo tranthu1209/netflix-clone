c879841a8c7fa516e93ccb685e82a781937a9c0a
tranthu1209 <77615114+tranthu1209@users.noreply.github.com>
2022-05-27 19:31:48 +0700

    fix responsive website and list button

--- frontend/src/component/list/List.jsx
+++ frontend/src/component/list/List.jsx
@@ -6,11 +6,11 @@ import { useEffect, useRef, useState } from 'react'
 const List = ({ list }) => {
   const [slideNumber, setSlideNumber] = useState(0);
   const [clickLimit, setClickLimit] = useState(window.innerWidth / 230);
-  const [isMoved, setIsMoved] = useState(false);
+  const nextRef = useRef();
+  const preRef = useRef();
   const listRef = useRef();
-  
+
   const handleClick = (direction) => {
-    setIsMoved(true);
     let distance = listRef.current.getBoundingClientRect().x - 50;
     if (direction === "left" && slideNumber > 0) {
       setSlideNumber(slideNumber - 1);
@@ -20,25 +20,29 @@ const List = ({ list }) => {
       setSlideNumber(slideNumber + 1);
       listRef.current.style.transform = `translateX(${-230 + distance}px)`;
     }
+    console.log(list.content.length - clickLimit, slideNumber)
   }
   return (
     <div className='list'>
       <span className="listTitle">{list.title}</span>
       <div className="wrapper">
         <ArrowBackIosOutlined className='sliderArrow left'
+          ref={preRef}
           onClick={() => handleClick('left')}
-          style={{ display: !isMoved && "none" }}
+          style={{ display: (slideNumber===0) && "none" }}
         />
         <div className="container" ref={listRef}>
           {
             list.content.map((item) => {
-              return <ListItem key={item} item={item}/>
+              return <ListItem key={item} item={item} />
             })
           }
 
         </div>
         <ArrowForwardIosOutlined className='sliderArrow right'
+          rel={nextRef}
           onClick={() => handleClick('right')}
+          style={{ display: (slideNumber > list.content.length - clickLimit) && "none" }}
         />
       </div>
     </div>
